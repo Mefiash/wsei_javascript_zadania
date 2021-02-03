1e70c09f27e001671af6a786b1980ff6ae5bdf8a
Adrian Zaczek <44535098+Mefiash@users.noreply.github.com>
2021-02-03 19:44:16 +0100

    1

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,24 @@
+// Z A D A N I E  1
+
+let a = 6;
+let imie = "Marcin";
+let b = 2 + "dwa";
+let flag = true ;
+let c = null;
+
+console.log(a);
+console.log(imie);
+console.log(b);
+console.log(flag);
+console.log(c);
+
+// Z A D A N I E  2
+const a = 2;
+const b = 6;
+let sum = 0;
+sum = a+b;
+console.log(sum);
+
+// Z A D A N I E  3
+let d;
+console.log(d);//undefined, ponieważ nie ma przypisanej wartości.
